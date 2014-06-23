3101c7bf46d94daaa3e08a6c5e6c160d416418ff
pgdoval <pablogonzalezdoval@hotmail.com>
2014-06-23 16:18:46 +0200

    Comprobamos la diferencia entre find y findAll. Comprobamos también que no se puede hacer un findWhere filtrando por atributos que no estén en la clase (algo del tipo librosEscritos.editorial)

--- grails-app/controllers/tutorial/LibrosController.groovy
+++ grails-app/controllers/tutorial/LibrosController.groovy
@@ -25,5 +25,48 @@ class LibrosController {
         }
         
     }
+        
+    def escritoresDeEditorialConFind(){
+        //vamos a obtener las personas que han trabajado con una editorial
+        // usando para ello HQL con una find -> esto sólo nos mostrará el primero
+        //al usar un método tipo find, al hacer el join contra librosEscritos, también se trae el libro
+        
+        List<Persona> personas = Persona.find("from Persona p inner join p.librosEscritos as e where e.editorial = :editorial",
+            [editorial: params.editorial]);
+        personas.each(){
+            render(it)
+        }
+        
+    }
+            
+    def escritoresDeEditorialConFindAll(){
+        //vamos a obtener las personas que han trabajado con una editorial
+        // usando para ello HQL con una findAll, que nos trae todos
+        //al usar un método tipo find, al hacer el join contra librosEscritos, también se trae el libro
+        
+        List<Persona> personas = Persona.findAll("from Persona p inner join p.librosEscritos as e where e.editorial = :editorial",
+            [editorial: params.editorial]);
+        personas.each(){
+            render(it)
+        }
+        
+    }            
+    
+    def escritoresDeEditorialConFindWhere(){
+        //esto no se puede hacer así porque no se puede
+        //acceder con un findWhere a las propiedades de una propiedad que no es
+        //de la clase sino que se tendría que acceder directamente a las de la clase
+//        List<Persona> personas = Persona.findWhere(
+//            librosEscritos.editorial : params.editorial
+//        )
+
+//            personas.each(){
+//            render(it)
+//        }
+        
+    }
+
+    
+    
     
 }
